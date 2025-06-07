fe9ffa83536da972ee0c6c650160d16e154becdd
Abdubek <zholay.abdubek@gmail.com>
2025-06-07 12:47:37 +0500

    fix

--- src/app/cabinet/(reports)/page.tsx
+++ src/app/cabinet/(reports)/page.tsx
@@ -1,9 +1,18 @@
 import { ReportsTable } from "@/modules/reports/table";
+import { Suspense } from "react";
+
+function ReportsTableWithSuspense() {
+  return (
+    <Suspense fallback={<div className="p-6"></div>}>
+      <ReportsTable />
+    </Suspense>
+  );
+}
 
 export default async function Page() {
   return (
     <div className="p-6">
-      <ReportsTable />
+      <ReportsTableWithSuspense />
     </div>
   );
 }
