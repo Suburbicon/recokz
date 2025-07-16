13e4faddfcf9a05ef501f02e641c22181dc98f84
Khassenov Dmitriy <support@reco.kz>
2025-07-16 11:46:18 +0500

    feat: add logs

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -5,14 +5,19 @@ export async function POST(req: NextRequest) {
   const data = await req.json();
 
   // Пример: создать транзакцию
-  await prisma.transaction.create({
-    data: {
-      amount: 100,
-      date: new Date(),
-      meta: data,
-      documentId: "some-id",
-    },
-  });
+  try {
+    await prisma.transaction.create({
+      data: {
+        amount: 100,
+        date: new Date(),
+        meta: data,
+        documentId: "some-id",
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ success: false }, { status: 500 });
+  }
 
-  return NextResponse.json({ success: true });
+  return NextResponse.json({ success: true }, { status: 200 });
 }
