35d98f10a3ee5a0c7210521b3fe8ce3e92f6bd5c
Khassenov Dmitriy <support@reco.kz>
2025-07-16 11:50:37 +0500

    feat: add doc id

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/shared/lib/prisma";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -11,12 +12,12 @@ export async function POST(req: NextRequest) {
         amount: 100,
         date: new Date(),
         meta: data,
-        documentId: "some-id",
+        documentId: "a9a14115-07b3-43b1-81c8-c06af0236014",
       },
     });
   } catch (e) {
     console.error(e);
-    return NextResponse.json({ success: false }, { status: 500 });
+    return NextResponse.json({ error: e }, { status: 500 });
   }
 
   return NextResponse.json({ success: true }, { status: 200 });
