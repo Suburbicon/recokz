08a7deb0b6007373c821e7cdb00a920980ac7467
Khassenov Dmitriy <support@reco.kz>
2025-08-14 14:15:02 +0500

    chore: merge develop

--- src/app/api/webhook/[companyId]/route.ts
+++ src/app/api/webhook/[companyId]/route.ts
@@ -10,16 +10,18 @@ export async function POST(
   const { companyId } = params;
   
   try {
-    await prisma.crmTransaction.create({
-      data: {
-        amount: data.data.amount,
-        date: new Date(data.data.date),
-        meta: data,
-        transactionId: data.data.document_id.toString() || '0',
-        organizationId: companyId,
-        bankTransactionId: null
-      },
-    });
+    if (data.resource === 'finances_operation' && data.status === 'create') {
+      await prisma.crmTransaction.create({
+        data: {
+          amount: data.data?.amount,
+          date: new Date(data.data?.date),
+          meta: data,
+          transactionId: data.data?.document_id?.toString() || '0',
+          organizationId: companyId,
+          bankTransactionId: null
+        },
+      });
+    }
   } catch (e) {
     console.error(e);
     return NextResponse.json({ error: e }, { status: 500 });
