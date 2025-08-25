b783d3daaddbd9bb2497639f09c6b9a1761fd513
Khassenov Dmitriy <support@reco.kz>
2025-08-25 17:31:06 +0500

    add account title

--- src/app/api/webhook/[companyId]/route.ts
+++ src/app/api/webhook/[companyId]/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/shared/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
+import dayjs from 'dayjs'
 
 export async function POST(
   req: NextRequest,
@@ -8,30 +9,26 @@ export async function POST(
   const data = await req.json();
   const params = await context.params;
   const { companyId } = params;
+
+  const parsedDate = dayjs(data.data?.date);
+  
+  const parsedResultDate = parsedDate.isValid()
+    ? parsedDate.toISOString()
+    : dayjs().toISOString();
   
   try {
-    // if (data.resource === 'finances_operation' && data.status === 'create') {
-    //   await prisma.crmTransaction.create({
-    //     data: {
-    //       amount: data.data?.amount.toString(),
-    //       date: new Date(data.data?.date),
-    //       meta: data,
-    //       transactionId: data.data?.document_id?.toString() || '0',
-    //       organizationId: companyId,
-    //       bankTransactionId: null
-    //     },
-    //   });
-    // }
-    await prisma.crmTransaction.create({
-      data: {
-        amount: data.data?.amount?.toString() || '0',
-        date: new Date(data.data?.date),
-        meta: data,
-        transactionId: data.data?.document_id?.toString() || '0',
-        organizationId: companyId,
-        bankTransactionId: null
-      },
-    });
+    if (data.resource === 'finances_operation' && data.status === 'create') {
+      await prisma.crmTransaction.create({
+        data: {
+          amount: data.data?.amount?.toString() || '-',
+          date: parsedResultDate,
+          meta: data,
+          transactionId: data.data?.document_id?.toString() || '0',
+          organizationId: companyId,
+          bankTransactionId: null
+        },
+      });
+    }
   } catch (e) {
     console.error(e);
     return NextResponse.json({ error: e }, { status: 500 });
