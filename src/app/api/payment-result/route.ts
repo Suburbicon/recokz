df4558c847305f6c43defd7f496f57ab9f5eebfe
Khassenov Dmitriy <support@reco.kz>
2025-08-25 19:48:47 +0500

    add payment result halyk

--- src/app/api/payment-result/route.ts
+++ src/app/api/payment-result/route.ts
@@ -6,38 +6,68 @@ import dayjs from 'dayjs';
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { result, organization_id, transaction_id } = body;
+    const { result, organization_id, transaction_id, type } = body;
 
     if (!organization_id || !result) {
       return NextResponse.json({ message: 'organizationId and result are required' }, { status: 400 });
     }
 
-    const parsedDate = dayjs(result.pos_response.data.chequeInfo.date);
+    if (type === 'kaspi') {
+      const parsedDate = dayjs(result.pos_response.data.chequeInfo.date);
 
-    const parsedResultDate = parsedDate.isValid()
-      ? parsedDate.toISOString()
-      : dayjs().toISOString();
+      const parsedResultDate = parsedDate.isValid()
+        ? parsedDate.toISOString()
+        : dayjs().toISOString();
 
-    const bankT = await prisma.bankTransaction.create({
-      data: {
-        amount: result.pos_response.data.chequeInfo.amount,
-        date: parsedResultDate,
-        meta: result.pos_response.data,
-        organizationId: organization_id as string,
-        transactionId: result.pos_response.data.transactionId
-      }
-    })
-    if (bankT) {
-      await prisma.crmTransaction.update({
-        where: {
-          id: transaction_id
-        },
+      const bankT = await prisma.bankTransaction.create({
         data: {
-          bankTransactionId: bankT.id
+          amount: result.pos_response.data.chequeInfo.amount,
+          date: parsedResultDate,
+          meta: result.pos_response.data,
+          organizationId: organization_id as string,
+          transactionId: result.pos_response.data.transactionId
         }
       })
-    } else {
-      throw Error('Произошла ошибка с созданием банковской транзакции')
+      if (bankT) {
+        await prisma.crmTransaction.update({
+          where: {
+            id: transaction_id
+          },
+          data: {
+            bankTransactionId: bankT.id
+          }
+        })
+      } else {
+        throw Error('Произошла ошибка с созданием банковской транзакции')
+      }
+    } else if (type === 'halyk') {
+      const parsedDate = dayjs(result.pos_response.data.dateTime);
+
+      const parsedResultDate = parsedDate.isValid()
+        ? parsedDate.toISOString()
+        : dayjs().toISOString();
+
+      const bankT = await prisma.bankTransaction.create({
+        data: {
+          amount: result.pos_response.data.amount,
+          date: parsedResultDate,
+          meta: result.pos_response.data,
+          organizationId: organization_id as string,
+          transactionId: crypto.randomUUID()
+        }
+      })
+      if (bankT) {
+        await prisma.crmTransaction.update({
+          where: {
+            id: transaction_id
+          },
+          data: {
+            bankTransactionId: bankT.id
+          }
+        })
+      } else {
+        throw Error('Произошла ошибка с созданием банковской транзакции')
+      }
     }
 
     return NextResponse.json({ message: 'Command sent to agent successfully', status: 'wait' }, { status: 200 });
