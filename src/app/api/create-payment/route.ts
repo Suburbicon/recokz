a0700d4acf1b64ff26976eec1c5421a7f644f324
Khassenov Dmitriy <support@reco.kz>
2025-08-25 20:41:03 +0500

    fix channel name

--- src/app/api/create-payment/route.ts
+++ src/app/api/create-payment/route.ts
@@ -18,7 +18,8 @@ export async function POST(request: Request) {
       return NextResponse.json({ message: 'organizationId and amount are required' }, { status: 400 });
     }
 
-    const channelName = `public-store-${companyId}`;
+    const channelName = 'public-store-001';
+    // const channelName = `public-store-${companyId}`;
     const eventName = `create-payment-command-${type}`;
     const payload = { 
         amount,
